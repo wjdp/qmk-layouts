77ba7df6668e808ed3d1627b5db89f8331341c70
Will Pimblett <will@wegift.io>
2023-12-07 11:57:11 +0000

    Continual improvement

--- keymap.c
+++ keymap.c
@@ -16,7 +16,6 @@ enum layers {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
-
 void set_backlight_for_layer(uint8_t hue, uint8_t sat, uint8_t val) {
   rgblight_enable_noeeprom();
   rgblight_sethsv_noeeprom(hue, sat, val);
@@ -24,26 +23,6 @@ void set_backlight_for_layer(uint8_t hue, uint8_t sat, uint8_t val) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   layer_state_t adjusted_state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-  switch(biton32(adjusted_state)) {
-    case _LOWER:
-      set_backlight_for_layer(HSV_GREEN);
-      break;
-    case _RAISE:
-      set_backlight_for_layer(HSV_YELLOW);
-      break;
-    case _ADJUST:
-      set_backlight_for_layer(HSV_BLUE);
-      break;
-    case _WM:
-      set_backlight_for_layer(HSV_MAGENTA);
-      break;
-    case _MOUSE:
-      set_backlight_for_layer(HSV_CYAN);
-      break;
-    default:
-      rgb_matrix_sethsv_noeeprom(HSV_OFF);
-      break;
-  }
   return adjusted_state;
 }
 
@@ -79,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN,    KC_BSPC,
   ESCKEY,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,       KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_ENT ,
-  CAPSKEY, KC_LALT, KC_LGUI, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   MO(_WM), _______, OSM(KC_MEH),KC_RGHT
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_ENT,
+  OSM_SFT, KC_LALT, KC_LGUI, KC_LCTL, LOWER,   KC_SPC,  KC_SPC,  RAISE,   MO(_WM), _______, _______,    KC_RGHT
 ),
 [_LOWER] = LAYOUT_ortho_4x12(
-  UK_GRV,   KC_EXLM,  KC_HOME,       KC_UP,    KC_END,         TD(TD_SYM3), TD(TD_SYM4), KC_AMPR,  KC_ASTR,   KC_LCBR,        KC_RCBR,      _______,
-  KC_BSPC,  KC_ENTER, KC_LEFT,       KC_DOWN,  KC_RIGHT,       KC_UNDS,     KC_COLN,     KC_MINUS, KC_EQUAL,  KC_LPRN,        KC_RPRN,      UK_DQUO,
-  _______,  UK_BSLS,  LCTL(KC_LEFT), XXXXXXX,  LCTL(KC_RIGHT), UK_PIPE,     KC_PLUS,     UK_HASH,  UK_TILD,   TD(TD_LDBR),    TD(TD_RDBR),  _______,
-  _______,  _______,  _______,       _______,  _______,        _______,     _______,     _______,  _______,   LSFT(KC_RALT),  _______,      CW_TOGG
+  UK_GRV,   KC_EXLM,  KC_HOME,       KC_UP,    KC_END,         TD(TD_SYM3), TD(TD_SYM4), KC_AMPR,  KC_ASTR,       KC_LCBR,     KC_RCBR,      _______,
+  KC_BSPC,  KC_ENTER, KC_LEFT,       KC_DOWN,  KC_RIGHT,       KC_UNDS,     KC_COLN,     KC_MINUS, KC_EQUAL,      KC_LPRN,     KC_RPRN,      UK_DQUO,
+  _______,  UK_BSLS,  LCTL(KC_LEFT), XXXXXXX,  LCTL(KC_RIGHT), UK_PIPE,     KC_PLUS,     UK_HASH,  UK_TILD,       TD(TD_LDBR), TD(TD_RDBR),  _______,
+  _______,  _______,  _______,       _______,  _______,        _______,     _______,     _______,  LSFT(KC_RALT), _______,     _______,      CW_TOGG
 ),
 [_RAISE] = LAYOUT_ortho_4x12(
   _______,  KC_F1,    KC_F2,         KC_F3,    KC_F4,          KC_NO,       KC_KP_ASTERISK, KC_7,     KC_8,  KC_9,   KC_KP_MINUS,  _______,
@@ -95,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  _______,  _______,       _______,  _______,        _______,     _______,        _______,  KC_0,  KC_DOT, _______,      KC_CAPS_LOCK
 ),
 [_ADJUST] = LAYOUT_ortho_4x12(
-  _______, I3_QUIT, I3_WS_L, I3_WN_U, I3_WS_R, _______, _______, _______, _______, _______, KC_INS,  KC_DELETE,
+  _______, I3_QUIT, I3_WS_L, I3_WN_U, I3_WS_R, _______, _______, _______, _______, _______, KC_INS,  _______,
   QK_BOOT, _______, I3_WN_L, I3_WN_D, I3_WN_R, _______, KC_VOLU, KC_LSFT, KC_LCTL, _______, KC_PSCR, SN_0002,
-  _______, AU_ON,   AU_OFF,  CK_TOGG, CK_UP,   CK_DOWN, KC_VOLD, _______, _______, _______, _______, SN_0003,
+  _______, AU_ON,   AU_OFF,  _______, _______, _______, KC_VOLD, _______, _______, _______, _______, SN_0003,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SN_0004
 ),
 [_WM] = LAYOUT_ortho_4x12(
@@ -123,73 +102,62 @@ void keyboard_post_init_user(void) {
 // I assume the first 6 are on the back of the board
 
 // Define some colours as 3 int arrays
+#define _PASS_ {-1, -1, -1}
 #define CL_OFF {0, 0, 0}
 #define CL_RED {168, 18, 10}
 #define CL_GRN {21, 163, 33}
 #define CL_BLU {35, 57, 219}
 #define CL_YEL {238, 242, 7}
+#define CL_MAG {255, 0, 255}
 #define CL_FFF {255, 255, 255}
-
-// Set KEY_LED_TOTAL to the number of keys 48
-#define KEY_LED_TOTAL 48
-#define LED_INDEX_OFFSET 6
+#define CL_CYN {0, 255, 255}
 
 // Define an 2d array of RGB LED colours with rows and columns
 const uint8_t PROGMEM colourmaps[][MATRIX_ROWS][MATRIX_COLS][3] = {
   [_QWERTY] = {
     {CL_BLU, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_RED}, 
     {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_BLU}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_BLU}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
   }, 
   [_LOWER] = {
-    {CL_GRN, CL_GRN, CL_BLU, CL_YEL, CL_BLU, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_RED}, 
-    {CL_RED, CL_BLU, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_GRN, CL_YEL, CL_OFF, CL_YEL, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_BLU}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {CL_GRN, CL_GRN, CL_BLU, CL_YEL, CL_BLU, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_RED}, 
+    {CL_RED, CL_BLU, CL_YEL, CL_YEL, CL_YEL, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN}, 
+    {_PASS_, CL_GRN, CL_YEL, CL_OFF, CL_YEL, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_GRN, CL_BLU}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_GRN, CL_GRN, CL_GRN, CL_OFF, CL_YEL, CL_OFF, CL_OFF, {RGB_MAGENTA}}, 
   }, 
   [_RAISE] = {
-    {CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_OFF, CL_FFF, CL_FFF, CL_FFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_OFF, CL_FFF, CL_FFF, CL_FFF, CL_FFF, CL_OFF}, 
-    {CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_OFF, CL_FFF, CL_FFF, CL_FFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_FFF, CL_FFF, CL_OFF, CL_OFF}, 
+    {CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_GRN, CL_FFF, CL_FFF, CL_FFF, CL_GRN, CL_RED}, 
+    {CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_GRN, CL_FFF, CL_FFF, CL_FFF, CL_FFF, CL_RED}, 
+    {_PASS_, CL_YEL, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_GRN, CL_FFF, CL_FFF, CL_FFF, CL_GRN, CL_BLU}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_FFF, CL_FFF, CL_OFF, {RGB_YELLOW}}, 
   },
   [_ADJUST] = {
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {CL_OFF, CL_RED, CL_FFF, CL_BLU, CL_FFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_FFF, CL_OFF}, 
+    {CL_RED, CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_OFF, CL_OFF, CL_FFF, CL_FFF, CL_OFF, CL_FFF, CL_OFF}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
   },
   [_WM] = {
     {CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF, CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF}, 
-    {CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF, CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {CL_OFF, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_BLU, CL_OFF}, 
+    {CL_FFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_CYN}, 
+    {CL_OFF, CL_OFF, CL_OFF, CL_FFF, CL_MAG, CL_MAG, CL_MAG, CL_RED, CL_MAG, CL_OFF, CL_OFF, CL_OFF}, 
   },
   [_MOUSE] = {
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
-    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_FFF, CL_YEL, CL_FFF, CL_BLU, CL_OFF}, 
+    {CL_YEL, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_FFF, CL_YEL, CL_YEL, CL_YEL, CL_BLU, CL_OFF}, 
+    {_PASS_, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_OFF, CL_CYN, CL_CYN, CL_OFF, CL_OFF, CL_OFF, CL_OFF}, 
+    {CL_OFF, CL_OFF, CL_MAG, CL_MAG, CL_MAG, CL_OFF, CL_YEL, CL_YEL, CL_YEL, CL_OFF, CL_OFF, CL_OFF}, 
   },
 };
 
-// void apply_layer_default(void) {
-//   // for (int i = 0; i < KEY_LED_TOTAL; i++) {
-//   //   rgb_matrix_set_color(i + LED_INDEX_OFFSET, colour_layer_default[i][0], colour_layer_default[i][1], colour_layer_default[i][2]);
-//   // }
-//   for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-//     for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-//         uint8_t index = g_led_config.matrix_co[row][col];
-//         rgb_matrix_set_color(index, colour_layer_default[1][0], colour_layer_default[1][1], colour_layer_default[1][2]);
-//     }
-//   }
-// }
+bool caps_word_active = false;
+void caps_word_set_user(bool active) {caps_word_active = active;}
 
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//   apply_layer_default();
-//   return false;
-// }
+uint8_t mods_active = 0;
+void oneshot_mods_changed_user(uint8_t mods) {mods_active = mods;}
+
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   // loop over all values between led_min and led_max
@@ -197,6 +165,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   int layer = get_highest_layer(layer_state);
   for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
     for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+        if (colourmaps[layer][row][col][0] == -1) continue; // skip if no colour (i.e. _PASS_)
         uint8_t index = g_led_config.matrix_co[row][col];
         rgb_matrix_set_color(
           index, 
@@ -204,51 +173,21 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         );
     }
   }
+  uint8_t capsIndex = g_led_config.matrix_co[2][0];
+  if (caps_word_active) {
+    rgb_matrix_set_color(capsIndex, RGB_MAGENTA);
+  } else if (host_keyboard_led_state().caps_lock) {
+    rgb_matrix_set_color(capsIndex, RGB_YELLOW);
+  } else {
+    rgb_matrix_set_color(capsIndex, RGB_OFF);
+  }
+
+  uint8_t osmShiftIndex = g_led_config.matrix_co[3][0];
+  if (mods_active & MOD_MASK_SHIFT) {
+    rgb_matrix_set_color(osmShiftIndex, RGB_MAGENTA);
+  } else {
+    rgb_matrix_set_color(osmShiftIndex, RGB_OFF);
+  }
+
   return false;
 }
-
-
-
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(0, 255, 0, 0);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(6, 0, 255, 0);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(10, 255, 0, 0);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(12, 0, 0, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(18, 255, 0, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(24, 0, 255, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(30, 255, 255, 0);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(36, 255, 255, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(42, 255, 255, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(48, 255, 255, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(54, 255, 255, 255);
-//     // RGB_MATRIX_INDICATOR_SET_COLOR(60, 255, 255, 255);
-
-//     rgb_matrix_set_color(6, 0, 0, 255);
-//     rgb_matrix_set_color(7, 0, 0, 255);
-//     rgb_matrix_set_color(8, 255, 255, 255);
-//     rgb_matrix_set_color(9, 255, 255, 255);
-//     rgb_matrix_set_color(10, 0, 0, 255);
-
-
-//     // apply_layer_default();
-
-//     return false;
-// }
-
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     if (get_highest_layer(layer_state) > 0) {
-//         uint8_t layer = get_highest_layer(layer_state);
-
-//         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-//             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-//                 uint8_t index = g_led_config.matrix_co[row][col];
-
-//                 if (index >= led_min && index < led_max && index != NO_LED &&
-//                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-//                     rgb_matrix_set_color(index, RGB_GREEN);
-//                 }
-//             }
-//         }
-//     }
-//     return false;
-// }
