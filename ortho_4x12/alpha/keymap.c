d8bc3522eb7e9a3c9b6e05b110d6f18846fed657
Will Pimblett <will@wegift.io>
2023-12-11 14:29:28 +0000

    Let's try home row mods again!

--- ortho_4x12/alpha/keymap.c
+++ ortho_4x12/alpha/keymap.c
@@ -58,10 +58,22 @@ tap_dance_action_t tap_dance_actions[] = {
 #define KP_EQL KC_KP_EQUAL
 #define KP_MINS KC_KP_MINUS
 
+// Mod tap keys, left hand
+#define MT_A LALT_T(KC_A)
+#define MT_S LGUI_T(KC_S)
+#define MT_D LCTL_T(KC_D)
+#define MT_F LSFT_T(KC_F)
+// Mod tap keys, right hand
+#define MT_J LSFT_T(KC_J)
+#define MT_K LCTL_T(KC_K)
+#define MT_L LGUI_T(KC_L)
+#define MT_P LALT_T(KC_P)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN, KC_BSPC,
-  ESCKEY,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,    KC_QUOT,
+  ESCKEY,  MT_A,    MT_S,    MT_D,    MT_F,    KC_G,    KC_H,    MT_J,    MT_K,    MT_L,    MT_P,    KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   OSM_SFT, KC_LALT, KC_LGUI, KC_LCTL, LOWER,   WP_SPCL, WP_SPCR, RAISE,   MO(_WM), XXXXXXX, XXXXXXX, KC_RGHT
 ),
