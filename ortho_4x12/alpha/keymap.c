c55848e37eefef27498c3de48d35a17cc199eed4
Will Pimblett <will@wjdp.uk>
2025-10-02 18:40:23 +0100

    Add some game lower keys

--- ortho_4x12/alpha/keymap.c
+++ ortho_4x12/alpha/keymap.c
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN, KC_BSPC,
   KC_ESC,  MT(MOD_LSFT, KC_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,    KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  XXXXXXX, XXXXXXX, KC_LALT, KC_LCTL, MO_GLOW, KC_SPC,  KC_SPC,  MO_GRSE, MO_WM,   XXXXXXX, XXXXXXX, I3_WN_R
+  KC_CAPS, XXXXXXX, KC_LALT, KC_LCTL, MO_GLOW, KC_SPC,  KC_SPC,  MO_GRSE, MO_WM,   XXXXXXX, XXXXXXX, I3_WN_R
 ),
 //  Tab  |    Q   |    W   |    E   |    R   |    T   |    Y   |    U   |    I   |    O   |    ;   |  Bksp
 //  Esc  |    A   |    S   |    D   |    F   |    G   |    H   |    J   |    K   |    L   |    P   |  "
@@ -193,7 +193,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME_LOWER] = LAYOUT_ortho_4x12(
   _______, KC_EXLM, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, _______, _______, _______,
   KC_BSPC, KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,
-  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______,
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_LBRC, KC_RBRC, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TO_HOME
 ),
 [_GAME_RAISE] = LAYOUT_ortho_4x12(
